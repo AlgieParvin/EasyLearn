82a5c6016441ee2fa60a344b1b1d75a91f2c10bb
AlgieParvin <pauchok97@gmail.com>
2016-11-14 10:27:07 +0200

    Basic tests functionality

--- EasyLearn/app/src/main/java/ua/nure/crew/easylearn/exceptions/ContentTypeException.java
+++
@@ -1,9 +0,0 @@
-package ua.nure.crew.easylearn.exceptions;
-
-/**
- * Created by Slava on 21.10.2016.
- */
-public class ContentTypeException extends Exception {
-
-    public ContentTypeException(String message) { super(message);}
-}
